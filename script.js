231a7d252fba7720e20c6f955ea2d17cab1f454e
originalmatis <brazinamatej@gmail.com>
2022-02-21 11:24:41 +0100

    pridal som zakladné premenné

---
+++ script.js
@@ -0,0 +1,16 @@
+const startBtn = document.querySelector("#button-start")
+const stopBtn = document.querySelector("#button-stop")
+const resetBtn = document.querySelector("#button-reset")
+const tens = document.querySelector("#tens")
+const sec = document.querySelector("#seconds")
+let tensValue = 00
+let secValue = 00
+let i = 0
+
+var intervalId = window.setInterval(function(){
+    tensValue++
+    if (tensValue < 10){
+        tensValue = "0" + tensValue
+    } 
+    
+}, 10);
