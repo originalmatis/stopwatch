82db0b06c499525e920d66e6fff6d6ec2d7e64dc
originalmatis <brazinamatej@gmail.com>
2022-02-21 13:04:29 +0100

    pridal som aby každých 100 tens pridala 1 sekunda

--- script.js
+++ script.js
@@ -3,14 +3,22 @@ const stopBtn = document.querySelector("#button-stop")
 const resetBtn = document.querySelector("#button-reset")
 const tens = document.querySelector("#tens")
 const sec = document.querySelector("#seconds")
-let tensValue = 00
-let secValue = 00
+let tensValue = 0
+let secValue = 0
 let i = 0
 
 var intervalId = window.setInterval(function(){
     tensValue++
     if (tensValue < 10){
         tensValue = "0" + tensValue
-        console.log(tensValue)
     }
+    if (tensValue == 100){
+        tensValue = 0
+        secValue++
+        if (secValue < 10){
+            secValue = "0" + secValue
+        }
+    }
+    tens.textContent = tensValue
+    sec.textContent = secValue
 }, 10);
